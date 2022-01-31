05e8008edfd580cc90604df9e03e78ed6b100600
Ricardo V <ricardo.v.ramirez@gmail.com>
2022-01-31 08:20:33 +0100

    Delete databse directory
    Contiene info sensible

--- database/seeders/ficheros_bd/areas.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `areas` (`id`, `area`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'Tráfico', NULL, NULL, NULL),
-	(2, 'Industrial', NULL, NULL, NULL),
-	(3, 'Fondo', NULL, NULL, NULL),
-	(4, 'N/A', NULL, NULL, NULL);
--- database/seeders/ficheros_bd/zones.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `zones` (`id`, `zone`, `created_at`, `updated_at`, `deleted_at`, `network_id`) VALUES
-	(1, 'ES1308 Aglomeración Corredor del Henares', NULL, NULL, NULL, 1),
-	(2, 'ES1309 Aglomeración Urbana Sur', NULL, NULL, NULL, 1),
-	(3, 'ES1310 Aglomeración Urbana Noroeste', NULL, NULL, NULL, 1),
-	(4, 'ES1311 Rural Sierra Norte', NULL, NULL, NULL, 1),
-	(5, 'ES1312 Cuenca del Alberche', NULL, NULL, NULL, 1),
-	(6, 'ES1313 Cuenca del Tajuña', NULL, '2020-11-24 10:36:24', NULL, 1),
-	(7, 'N/A', NULL, NULL, NULL, 1);
